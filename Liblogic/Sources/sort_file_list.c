5854989799399d4e730208c87190e54e55b87c82
Nestor <nestoroprysk@gmail.com>
2018-10-05 02:44:06 +0300

    The work on partition is in progress

--- Liblogic/Sources/sort_file_list.c
+++ Liblogic/Sources/sort_file_list.c
@@ -9,32 +9,40 @@ void swap(t_file_node** a, t_file_node** b);
 
 void sort_file_list(t_file_list* dir_files)
 {
-	assert(dir_files);
+	assert(dir_files && dir_files->len > 1);
+	t_file_node prev_head;
+	t_file_node next_last;
+	dir_files->head->prev = &prev_head;
+	prev_head.next = dir_files->head;
+	dir_files->last->next = &next_last;
+	next_last.prev = dir_files->last;
 	sort(&dir_files->head, &dir_files->last);
+	dir_files->head = prev_head.next;
+	dir_files->head->prev = NULL;
+	dir_files->last = next_last.prev;
+	dir_files->last->next = NULL;
 }
 
 void sort(t_file_node** head_ptr, t_file_node** last_ptr)
 {
-	t_file_node prev_head;
-	t_file_node next_last;
-	(*head_ptr)->prev = &prev_head;
-	prev_head.next = *head_ptr;
-	(*last_ptr)->next = &next_last;
-	next_last.prev = *last_ptr;
-
+	assert(head_ptr && last_ptr && *head_ptr && *last_ptr);
 	t_file_node* mid = partition(head_ptr, last_ptr);
-	assert(mid);
-
-	*head_ptr = prev_head.next;
-	(*head_ptr)->prev = NULL;
-	*last_ptr = next_last.prev;
-	(*last_ptr)->next = NULL;
+	assert(mid);	
 }
 
 t_file_node* partition(t_file_node** head_ptr, t_file_node** last_ptr)
 {
 	assert(head_ptr && last_ptr && *head_ptr && *last_ptr);
-	swap_nodes(head_ptr, last_ptr);
+	t_file_node* pivot = *head_ptr;
+	t_file_node* it = pivot->next;
+	t_file_node* first_open = it;
+	while (it != *last_ptr)
+	{
+		if (ft_strcmp(it->info.name, pivot->info.name) < 0)
+			swap_nodes(&it, &first_open);
+		it = it->next;
+	}
+	swap_nodes(&pivot, &first_open);
 	return *head_ptr;
 }
 
