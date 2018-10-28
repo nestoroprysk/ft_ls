4d4bdce3e6ddab3af62714aaf731ea43bb1f53b6
Nestor <nestoroprysk@gmail.com>
2018-10-28 16:06:44 +0200

    Segfault cought

--- Liblogic/Sources/define_file_type.c
+++ Liblogic/Sources/define_file_type.c
@@ -9,19 +9,20 @@
 static bool is_root_dir(mode_t type, const char* file_name);
 static bool is_dir(mode_t type, const char* file_name);
 static bool is_reg_file(mode_t type, const char* file_name);
+static bool is_true(mode_t type, const char* file_name);
 
 typedef bool (*is_file_type_func)(mode_t s, const char* file_name);
 
 char get_file_type_char(t_file_node* n)
 {
-	static char file_type_char[NB_FILE_TYPES] = { 'd', 'd', '-' };
+	static char file_type_char[NB_FILE_TYPES] = { 'd', 'd', '-', '?' };
 	return file_type_char[define_file_type(n)];
 }
 
 enum file_type define_file_type(const t_file_node* n)
 {
 	static is_file_type_func is_file_type[NB_FILE_TYPES] =
-									{ is_root_dir, is_dir, is_reg_file };
+									{ is_root_dir, is_dir, is_reg_file, is_true };
 	assert(n);
 	mode_t type = n->raw_info.stat.st_mode;
 	for (size_t i = 0; i < NB_FILE_TYPES; ++i)
@@ -47,4 +48,11 @@ static bool is_reg_file(mode_t type, const char* file_name)
 {
 	(void)file_name;
 	return S_ISREG(type);
+}
+
+static bool is_true(mode_t type, const char* file_name)
+{
+	(void)type;
+	assert(file_name);
+	return true;
 }
